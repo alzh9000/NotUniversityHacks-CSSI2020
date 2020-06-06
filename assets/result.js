06d7e201035282ff49b3f13f1647286f02830a8c
Tiffany <cnnmonn@gmail.com>
2020-06-06 17:14:43 -0400

    fade in/out

--- assets/result.js
+++ assets/result.js
@@ -9,22 +9,22 @@ function initData() {
     getCountdown();
     $('.with-location').show();
   } else $('.without-location').show();
-  $("#blank").fadeOut(500);
+  $("#blank").delay(300).fadeOut(1000);
 }
 
 function getCountdown() {
   // Set the date to the next drought date
-  var countDownDate = new Date("June 7, 2022 18:37:25").getTime(); //TEMPORARY
+  let countDownDate = new Date("June 7, 2022 18:37:25").getTime(); //TEMPORARY
   // Update the count down every 1 second
-  var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365));
-    var months = Math.floor((distance % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * (365 / 12)));
-    var days = Math.floor((distance % (1000 * 60 * 60 * 24 * (365 / 12))) / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let x = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+    let years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365));
+    let months = Math.floor((distance % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * (365 / 12)));
+    let days = Math.floor((distance % (1000 * 60 * 60 * 24 * (365 / 12))) / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Output the result in an element with id="timer"
     document.getElementById("timer").innerHTML = years + "y " + months + "m " + days + "d " + hours + "h " +
