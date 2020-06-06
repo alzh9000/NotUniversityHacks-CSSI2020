5a02b58faa691b04600a6e05c6632892158e2ed6
alzh9000 <32472855+alzh9000@users.noreply.github.com>
2020-06-06 19:21:08 -0400

    cleaning up the code for the AI that I was just experimenting with

--- drought_data/time_series_AI.py
+++ drought_data/time_series_AI.py
@@ -4,24 +4,8 @@ import matplotlib.pyplot as plt
 from statsmodels.tsa.seasonal import seasonal_decompose
 
 df = pd.read_csv("""drought_data\State Data\california.csv""", parse_dates=['MapDate'], index_col='MapDate')
-print(df.head())
-# print(df.describe())
-
-def plot_df(df, x, y, title="", xlabel='Date', ylabel='Value', dpi=100):
-    plt.figure(figsize=(16,5), dpi=dpi)
-    plt.plot(x, y, color='tab:red')
-    plt.gca().set(title=title, xlabel=xlabel, ylabel=ylabel)
-    plt.show()
-
-print(df.columns)
-drought_type = df.D0
-#plot_df(df, x=df.index, y=drought_type, title='Drought data from 2000 to 2020')
-
 
 drought_type_string = 'D1'
-
-# Multiplicative Decomposition 
-# result_mul = seasonal_decompose(df[drought_type_string], model='multiplicative', extrapolate_trend='freq')
 
 # Additive Decomposition. 
 result_add = seasonal_decompose(df[drought_type_string], model='additive', extrapolate_trend='freq')
@@ -39,29 +23,3 @@ plt.rcParams.update({'figure.figsize': (10,10)})
 # result_mul.plot().suptitle('Multiplicative Decompose', fontsize=22)
 result_add.plot().suptitle('Additive Decompose', fontsize=22)
 plt.show()
-
-# # seeing what the drought data looked like
-# # droughts = ['D1', 'D2', 'D3', 'D4']
-# for drought_type_string in droughts:
-
-#   # drought_type_string = 'D1'
-
-#   # Multiplicative Decomposition 
-#   # result_mul = seasonal_decompose(df[drought_type_string], model='multiplicative', extrapolate_trend='freq')
-
-#   # Additive Decomposition. 
-#   result_add = seasonal_decompose(df[drought_type_string], model='additive', extrapolate_trend='freq')
-
-#   # Returns a <class 'pandas.core.series.Series'>
-#   print(type(result_add.seasonal))
-
-#   seasonal_df = result_add.seasonal.to_frame()
-#   print(seasonal_df)
-
-#   print(seasonal_df.describe())
-
-#   # Plot
-#   plt.rcParams.update({'figure.figsize': (10,10)})
-#   # result_mul.plot().suptitle('Multiplicative Decompose', fontsize=22)
-#   result_add.plot().suptitle('Additive Decompose', fontsize=22)
-#   plt.show()
