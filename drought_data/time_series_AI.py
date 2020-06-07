6ecae479d4a9abf49efaae6ec53c00b925f42780
alzh9000 <32472855+alzh9000@users.noreply.github.com>
2020-06-06 22:38:14 -0400

    testing the AI on all the files

--- drought_data/time_series_AI.py
+++ drought_data/time_series_AI.py
@@ -1,49 +1,59 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
 from scipy.optimize import curve_fit
 from statsmodels.tsa.seasonal import seasonal_decompose
 from random import seed
 from random import random
+from glob import glob
 
-df = pd.read_csv("""drought_data\State Data\\georgia.csv""", parse_dates=['MapDate'], index_col='MapDate')
+seed(1)
 
-drought_type_string = 'D1'
+with open("ai_predictions.csv", 'w') as file:
+  fields = ('State', 'Years until next drought')
+  writer = csv.DictWriter(file, fieldnames=fields, lineterminator = '\n')
+  writer.writeheader()
+  
+  for path_name in glob("""drought_data\State Data\*.csv"""):
+    
+    
+    
+    df = pd.read_csv(path_name, parse_dates=['MapDate'], index_col='MapDate')
 
-# Additive Decomposition. 
-result_add = seasonal_decompose(df[drought_type_string], model='additive', extrapolate_trend='freq')
+    drought_type_string = 'D1'
+
+    # Additive Decomposition. 
+    result_add = seasonal_decompose(df[drought_type_string], model='additive', extrapolate_trend='freq')
 
 
-seasonal_df = result_add.seasonal.to_frame()
+    seasonal_df = result_add.seasonal.to_frame()
 
-# Test function with coefficients as parameters 
-def test(x, a, b, c, d): 
-  return a * np.sin(b * x + c) + d
+    # Test function with coefficients as parameters 
+    def test(x, a, b, c, d): 
+      return a * np.sin(b * x + c) + d
 
-seasonal_df['dates'] = seasonal_df.index.values.astype(int)
+    seasonal_df['dates'] = seasonal_df.index.values.astype(int)
 
-xdata = seasonal_df['dates'].to_numpy()
-ydata = seasonal_df['seasonal'].to_numpy()
+    xdata = seasonal_df['dates'].to_numpy()
+    ydata = seasonal_df['seasonal'].to_numpy()
 
-print(xdata)
+    print(xdata)
 
-# curve_fit() function takes the test-function 
-# x-data and y-data as argument and returns  
-# the coefficients a and b in param and 
-# the estimated covariance of param in param_cov 
-# guesses guide the fitting function
-period_guess = random() * 10 + 5
-initial_guess = [10000, period_guess, 10, 10]
-param, param_cov = curve_fit(test, xdata, ydata, p0=initial_guess) 
+    # curve_fit() function takes the test-function 
+    # x-data and y-data as argument and returns  
+    # the coefficients a and b in param and 
+    # the estimated covariance of param in param_cov 
+    # guesses guide the fitting function
+    period_guess = random() * 25 + 3
+    initial_guess = [10000, period_guess, 10, 10]
+    param, param_cov = curve_fit(test, xdata, ydata, p0=initial_guess) 
 
-print("Sine funcion coefficients:") 
-print(param) 
-print("Covariance of coefficients:") 
-print(param_cov) 
+    print("Sine funcion coefficients:") 
+    print(param) 
+    print("Covariance of coefficients:") 
+    print(param_cov) 
 
-model = param[0] * np.sin(param[1] * xdata + param[2]) + param[3]
-# plt.plot(xdata, ydata, 'o', color ='red', label ="data")
-# plt.plot(xdata, model, '--', color ='blue', label ="model data")
-# plt.legend() 
-# plt.show()
+    writer.writerow({'State': "Georgia", 'Years until next drought': param[1]})
+  
