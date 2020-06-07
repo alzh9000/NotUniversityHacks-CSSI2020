9cec5e49e9a9dc9d4fff0d7400e50deb5ec2ee62
alzh9000 <32472855+alzh9000@users.noreply.github.com>
2020-06-06 22:19:50 -0400

    cleaned up the AI a bit

--- drought_data/time_series_AI.py
+++ drought_data/time_series_AI.py
@@ -14,34 +14,14 @@ drought_type_string = 'D1'
 # Additive Decomposition. 
 result_add = seasonal_decompose(df[drought_type_string], model='additive', extrapolate_trend='freq')
 
-# Returns a <class 'pandas.core.series.Series'>
-# print(type(result_add.seasonal))
 
 seasonal_df = result_add.seasonal.to_frame()
-# print(type(seasonal_df))
-# print(seasonal_df)
-
-# print(seasonal_df.describe())
-
-# plt.rcParams.update({'figure.figsize': (10,10)})
-# result_add.plot().suptitle('Additive Decompose', fontsize=22)
-# plt.show()
 
 # Test function with coefficients as parameters 
 def test(x, a, b, c, d): 
   return a * np.sin(b * x + c) + d
 
-print(seasonal_df.columns)
-
-print(seasonal_df.index)
-
 seasonal_df['dates'] = seasonal_df.index.values.astype(int)
-
-# print(type(seasonal_df['dates'][1]))
-
-
-# pd.to_datetime(seasonal_df['dates']).astype(int)
-# seasonal_df['dates'].str.replace("-","").astype(int)
 
 xdata = seasonal_df['dates'].to_numpy()
 ydata = seasonal_df['seasonal'].to_numpy()
@@ -52,6 +32,7 @@ print(xdata)
 # x-data and y-data as argument and returns  
 # the coefficients a and b in param and 
 # the estimated covariance of param in param_cov 
+# guesses guide the fitting function
 period_guess = random() * 10 + 5
 initial_guess = [10000, period_guess, 10, 10]
 param, param_cov = curve_fit(test, xdata, ydata, p0=initial_guess) 
@@ -62,7 +43,7 @@ print("Covariance of coefficients:")
 print(param_cov) 
 
 model = param[0] * np.sin(param[1] * xdata + param[2]) + param[3]
-plt.plot(xdata, ydata, 'o', color ='red', label ="data")
-plt.plot(xdata, model, '--', color ='blue', label ="model data")
-plt.legend() 
-plt.show()
+# plt.plot(xdata, ydata, 'o', color ='red', label ="data")
+# plt.plot(xdata, model, '--', color ='blue', label ="model data")
+# plt.legend() 
+# plt.show()
