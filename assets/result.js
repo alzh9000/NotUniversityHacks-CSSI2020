0b067c1c75460caf08ff35eb0471d9163739cc0a
Tiffany <cnnmonn@gmail.com>
2020-06-07 09:48:27 -0400

    Update result.js

--- assets/result.js
+++ assets/result.js
@@ -13,15 +13,13 @@ function initData() {
 }
 
 function getCountdown() {
-
-
-  // Set the date to the next drought date
-  let countDownDate = new Date("June 7, 2022").getTime(); //TEMPORARY
+  let now = new Date("June 7, 2020");
+  let year = now.getFullYear();
+  let countDownDate = new Date(year+9, now.getMonth(), now.getDay()).getTime(); //TEMPORARY
   // Update the count down every 1 second
   let x = setInterval(function() {
-    let now = new Date().getTime();
     let distance = countDownDate - now;
-    let years = Math.floor(24.359 / (1000 * 60 * 60 * 24 * 365));
+    let years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365));
     let months = Math.floor((distance % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * (365 / 12)));
     let days = Math.floor((distance % (1000 * 60 * 60 * 24 * (365 / 12))) / (1000 * 60 * 60 * 24));
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
